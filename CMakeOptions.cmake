31ecabc8d13a4ae6093400e5602b749ac61f2c3d
Sarnax <SarnaxOfficial@protonmail.com>
2022-07-16 01:21:38 +0700

    Add files via upload

---
+++ CMakeOptions.cmake
@@ -0,0 +1,20 @@
+###########
+#   User customizable options
+###########
+
+set(OS_ARCH "x86_64" CACHE STRING "Architecture of OS to be compiled")
+set(PROTO_DISTRO "stivale" CACHE STRING "Bootloader protocol to be used")
+list(APPEND CMMN_FLAGS 
+# Used to make linker be willing to put our code in high memory
+# alternatively we can use PIE and PIC magic
+-mcmodel=kernel
+-fno-pie
+# red zone is x86_64 abi thing, in kernel code it needs to be disabled because of interrupts
+-mno-red-zone
+-fno-stack-protector
+-fno-exceptions -fno-rtti -ffreestanding -fbuiltin -fno-threadsafe-statics -mgeneral-regs-only -mno-mmx -mno-sse -mno-sse2 -nostdlib)
+list(APPEND KERNEL_LINK_FLAGS -no-pie -ffreestanding -nostdlib)
+
+option(FORCE_DEBUG "Should asserts work even in release" ON)
+option(USE_LTO "Compile with LTO" OFF)
+option(COMPILE_TESTS "Compile Tests" ON)
