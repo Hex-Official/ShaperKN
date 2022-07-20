d55b26119a6ba8e072a9b04c76fa272b337ef81a
Sarnax <SarnaxOfficial@protonmail.com>
2022-07-21 05:03:31 +0700

    Update paging.cpp

--- kernel/arch/x86/src/paging.cpp
+++ kernel/arch/x86/src/paging.cpp
@@ -82,6 +82,27 @@ vmemCtxT initKernelVirtMem()
 	setContext(context);
 	return context;
 }
+	
+	void IOWait();
+	{
+void print(const int_8* str,uint_8 color = 0x0f);
+void Clearscr(uint_8 color = 0x0f);
+void setCursorpos(uint_16 pos);
+void outb8(uint_16 port,uint_8 value);
+void outb16(uint_16 port,uint_16 value);
+void outb32(uint_16 port,uint_32 value);
+void outbslow8(uint_16 port,uint_8 value);
+uint_8 inb8(uint_16 port);
+uint_16 inb16(uint_16 port);
+uint_32 inb32(uint_16 port);
+const char* hex2str(uint_64 value);
+const char* int2str(uint_64 value);
+	}
+
+
+		
+	
+	
 } // namespace x86
 
 namespace arch::vmem
